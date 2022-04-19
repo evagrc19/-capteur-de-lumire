892fe555bbde0fe93997f4dba9895d846d19791f
evagrc19 <103173881+evagrc19@users.noreply.github.com>
2022-04-19 13:52:03 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("" + (input.lightLevel()))
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-})
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        led.plotBarGraph(
+        input.lightLevel(),
+        255
+        )
+    } else {
+        if (input.buttonIsPressed(Button.B)) {
+            basic.clearScreen()
+        }
+    }
 })
